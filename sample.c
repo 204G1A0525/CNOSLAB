c3647c6006789e7d9c6821c6b0cd501fed5c8564
204G1A0525 <94513612+204G1A0525@users.noreply.github.com>
2022-10-11 11:00:22 +0530

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.chaitanya
+   Roll No: 204G1A0525
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
